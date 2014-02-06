9d705b5efc0a137f23229c39f4ba3cb4a2d89e4f
Till Maas <opensource@till.name>
2014-02-06 12:34:16 +0100

    PEP8 cnucnu.py

--- cnucnu.py
+++ cnucnu.py
@@ -20,6 +20,9 @@
 import logging
 import sys
 import os
+import pprint as pprint_module
+pp = pprint_module.PrettyPrinter(indent=4)
+pprint = pp.pprint
 
 import cnucnu
 import cnucnu.errors as cc_errors
@@ -29,9 +32,6 @@ from cnucnu.checkshell import CheckShell
 from cnucnu.bugzilla_reporter import BugzillaReporter
 from cnucnu.scm import SCM
 
-import pprint as pprint_module
-pp = pprint_module.PrettyPrinter(indent=4)
-pprint = pp.pprint
 
 log = logging.getLogger('cnucnu')
 
@@ -43,26 +43,33 @@ class Actions(object):
         repo = Repository(**global_config.config["repo"])
         scm = SCM(**global_config.config["scm"])
 
-        pl = PackageList(repo=repo, scm=scm, br=br, **global_config.config["package list"])
+        pl = PackageList(repo=repo, scm=scm, br=br,
+                         **global_config.config["package list"])
         package_count = len(pl)
         log.info("Checking '%i' packages", package_count)
         for number, package in enumerate(pl, start=1):
             if package.name >= args.start_with:
-                log.info("checking package '%s' (%i/%i)", package.name, number, package_count)
+                log.info("checking package '%s' (%i/%i)", package.name, number,
+                         package_count)
                 try:
                     if package.upstream_newer:
-                        print "package '%s' outdated (%s < %s)" % (package.name,
-                                                                   package.repo_version,
-                                                                   package.latest_upstream)
+                        print "package '%s' outdated (%s < %s)" % (
+                            package.name,
+                            package.repo_version,
+                            package.latest_upstream
+                        )
                         bug_url = package.report_outdated(dry_run=args.dry_run)
                         if bug_url:
                             print bug_url
                 except cc_errors.UpstreamVersionRetrievalError, e:
-                    log.error("Failed to fetch upstream information for package '%s' (%s)" % (package.name, e.message))
+                    log.error("Failed to fetch upstream information for "
+                              "package '%s' (%s)" % (package.name, e.message))
                 except cc_errors.PackageNotFoundError, e:
                     log.error(e)
                 except Exception, e:
-                    log.exception("Exception occured while processing package '%s':\n%s" % (package.name, pp.pformat(e)))
+                    log.exception("Exception occured while processing "
+                                  "package '%s':\n%s" % (package.name,
+                                                         pp.pformat(e)))
             else:
                 log.info("skipping package '%s'", package.name)
 
@@ -89,7 +96,9 @@ class Actions(object):
         possible_actions = {}
         for method in dir(self):
             if method.startswith("action_"):
-                possible_actions[method[len("action_"):].replace("_", "-")] = getattr(self, method).__doc__
+                possible_actions[
+                    method[len("action_"):].replace("_", "-")
+                ] = getattr(self, method).__doc__
         return possible_actions
 
     def action(self, action):
@@ -105,12 +114,22 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     actions = Actions()
 
-    parser.add_argument("--config", dest="config_filename", help="config_filename, e.g. for bugzilla credentials")
-    parser.add_argument("--dry-run", dest="dry_run", help="Do not file or change bugs", default=False, action="store_true")
-    parser.add_argument("--loglevel", dest="loglevel", help="Specify loglevel, default: %(default)s", choices=("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"), default="WARNING")
-    parser.add_argument("--start-with", dest="start_with", help="Start with this package when reporting bugs", metavar="PACKAGE", default="")
+    parser.add_argument("--config", dest="config_filename",
+                        help="config_filename, e.g. for bugzilla credentials")
+    parser.add_argument("--dry-run", dest="dry_run",
+                        help="Do not file or change bugs",
+                        default=False, action="store_true")
+    parser.add_argument("--loglevel", dest="loglevel",
+                        help="Specify loglevel, default: %(default)s",
+                        choices=("DEBUG", "INFO", "WARNING", "ERROR",
+                                 "CRITICAL"),
+                        default="WARNING")
+    parser.add_argument("--start-with", dest="start_with",
+                        help="Start with this package when reporting bugs",
+                        metavar="PACKAGE", default="")
 
-    subparsers = parser.add_subparsers(dest="action", help='command to perform')
+    subparsers = parser.add_subparsers(dest="action",
+                                       help='command to perform')
     possible_actions = actions.possible.items()
     possible_actions.sort()
     for action, help_text in possible_actions:
